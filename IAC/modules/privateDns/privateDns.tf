355dac026c60137825f1028224f00717ded00ba0
blai0624 <mblais93@gmail.com>
2025-04-30 15:57:25 -0400

    added privatelink_dns_name variable to modules

--- IAC/modules/privateDns/privateDns.tf
+++ IAC/modules/privateDns/privateDns.tf
@@ -1,17 +1,17 @@
 resource "azurerm_private_dns_zone" "privatelink_dns" {
-  name                = "privatelink.azurewebsites.net"
+  name                = "${var.platform}-${var.environment}.privatelink.azurewebsites.net"
   resource_group_name = var.private_dns_rg
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_dns_esdc_hub_peered_vnet_link" {
-  name                  = "vnet-link-esdc-hub-${var.environment}"
+  name                  = "vnet-link-${var.platform}-esdc-hub-vnet-${var.environment}"
   resource_group_name   = var.private_dns_rg
   private_dns_zone_name = azurerm_private_dns_zone.privatelink_dns.name
   virtual_network_id    = var.esdc_hub_peered_vnet_id
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_dns_vnet_link" {
-  name                  = "vnet-link-${var.platform}-${var.environment}"
+  name                  = "vnet-link-${var.platform}-vnet-${var.environment}"
   resource_group_name   = var.private_dns_rg
   private_dns_zone_name = azurerm_private_dns_zone.privatelink_dns.name
   virtual_network_id    = var.platform_vnet_id
