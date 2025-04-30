453e7b435b68c6cb39e21b43f13327d14e8f8da2
blai0624 <mblais93@gmail.com>
2025-04-30 16:48:09 -0400

    updated vnet link names

--- IAC/modules/privateDns/privateDns.tf
+++ IAC/modules/privateDns/privateDns.tf
@@ -4,14 +4,14 @@ resource "azurerm_private_dns_zone" "privatelink_dns" {
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_dns_esdc_hub_peered_vnet_link" {
-  name                  = "vnet-link-${var.platform}-esdc-hub-vnet-${var.environment}"
+  name                  = "link-to-${var.platform}-${var.environment}-esdc-hub-vnet"
   resource_group_name   = var.private_dns_rg
   private_dns_zone_name = azurerm_private_dns_zone.privatelink_dns.name
   virtual_network_id    = var.esdc_hub_peered_vnet_id
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_dns_vnet_link" {
-  name                  = "vnet-link-${var.platform}-vnet-${var.environment}"
+  name                  = "link-to-${var.platform}-${var.environment}-vnet"
   resource_group_name   = var.private_dns_rg
   private_dns_zone_name = azurerm_private_dns_zone.privatelink_dns.name
   virtual_network_id    = var.platform_vnet_id
